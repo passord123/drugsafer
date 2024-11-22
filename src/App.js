8d730bc1f6839e154b4a63726aece763988bcac6
passord123 <johanneslol@protonmail.com>
2024-11-22 18:31:28 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { AlertProvider } from './contexts/AlertContext/AlertProvider';
@@ -40,51 +39,15 @@ const App = () => {
     <AlertProvider>
       <Router>
         <div className="min-h-screen bg-gray-50">
-          {/* Header */}
-          <nav className="bg-white shadow-sm sticky top-0 z-50">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              <div className="flex justify-between h-16">
-                <div className="flex items-center">
-                  <Link to="/" className="flex-shrink-0 flex items-center space-x-2">
-                    <AlertTriangle className="h-8 w-8 text-blue-500" />
-                    <span className="text-xl font-bold text-gray-900">DrugSafe</span>
-                  </Link>
-                </div>
-
-                <div className="flex items-center space-x-2">
-                  <NavLink to="/" icon={Home}>Home</NavLink>
-                  <NavLink to="/drugs" icon={List}>My Drugs</NavLink>
-                  <NavLink to="/stats" icon={BarChart}>Stats</NavLink>
-                  <NavLink to="/add" icon={PlusCircle}>Add New</NavLink>
-                </div>
-              </div>
-            </div>
-          </nav>
-
           {/* Main Content */}
-          <main className="min-h-[calc(100vh-8rem)]">
-            <Router>
-              <div className="min-h-screen bg-gray-50">
-                <MobileOptimizedLayout>
-                  <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/drugs" element={<DrugListPage />} />
-                    <Route path="/add" element={<AddDrugPage />} />
-                    <Route path="/stats" element={<MedicationStats />} />
-                  </Routes>
-                </MobileOptimizedLayout>
-              </div>
-            </Router>
-          </main>
-
-          {/* Footer */}
-          <footer className="bg-white border-t h-16">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-full flex items-center justify-center">
-              <p className="text-gray-500 text-sm">
-                DrugSafe - Safer drug use
-              </p>
-            </div>
-          </footer>
+          <MobileOptimizedLayout>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/drugs" element={<DrugListPage />} />
+              <Route path="/add" element={<AddDrugPage />} />
+              <Route path="/stats" element={<MedicationStats />} />
+            </Routes>
+          </MobileOptimizedLayout>
         </div>
       </Router>
     </AlertProvider>
